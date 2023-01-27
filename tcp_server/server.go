cc0000a3ee118d65b5ec3adda0ffaf05a512df39
rebeccadjim <117664113+rebeccadjim@users.noreply.github.com>
2023-01-27 16:11:17 +0100

    Grayscale on tcp server loading

--- tcp_server/server.go
+++ tcp_server/server.go
@@ -12,12 +12,64 @@ import (
 	"strings"
 )
 
-var output_img *image.NRGBA
+var first_output_img *image.NRGBA
+var final_output_img *image.NRGBA
+
+var running bool
+
+type matrix_elem struct {
+	index_i int
+	index_j int
+}
+
+type Pixel struct {
+	index_i int
+	index_j int
+	pigment color.NRGBA
+}
+
+func map_int(to_map int, max_from int, max_to int) int {
+	return (to_map * max_to) / max_from
+}
+
+func grayscale_worker(jobChan chan matrix_elem, resultChan chan Pixel, loadedImage image.Image) {
+	//loops through the whole array (all the pixels of the image) and calculates the average value of r, g, b
+	//by setting the average value as the value for each color we have a grayscale image
+	for running {
+		elem := <-jobChan
+
+		colors := loadedImage.At(elem.index_i, elem.index_j)
+		red_input, green_input, blue_input, a_input := colors.RGBA()
+		red_input = uint32(map_int(int(red_input), 65535, 255))
+		green_input = uint32(map_int(int(green_input), 65535, 255))
+		blue_input = uint32(map_int(int(blue_input), 65535, 255))
+		a_input = uint32(map_int(int(a_input), 65535, 255))
+		avg := (red_input + green_input + blue_input) / 3
+
+		color := color.NRGBA{
+			R: uint8(avg),
+			G: uint8(avg),
+			B: uint8(avg),
+			A: uint8(a_input),
+		}
+		pixel := Pixel{
+			index_i: elem.index_i,
+			index_j: elem.index_j,
+			pigment: color,
+		}
+
+		resultChan <- pixel
+	}
+}
 
 func handle_connection(conn net.Conn) {
 	var data_array, msg_array []string
 	opened := true
 	var width, height int
+
+	numJobs := 20
+	jobChan := make(chan matrix_elem, numJobs)
+	resultChan := make(chan Pixel, numJobs)
 
 	for opened {
 		rec := make([]byte, 1024)
@@ -29,9 +81,9 @@ func handle_connection(conn net.Conn) {
 			opened = false
 		}
 		msg := string(rec)
-		 
+
 		data_array = strings.Split(msg, "\n")
-		
+
 		for i := 0; i < len(data_array); i++ {
 			s := data_array[i]
 			msg_array = strings.Split(s, " ")
@@ -47,7 +99,7 @@ func handle_connection(conn net.Conn) {
 				if err != nil {
 					fmt.Println("error converting height")
 				}
-				output_img = image.NewNRGBA(image.Rect(0, 0, width, height))
+				first_output_img = image.NewNRGBA(image.Rect(0, 0, width, height))
 
 			} else if len(msg_array) > 1 {
 				i, err := strconv.Atoi(msg_array[0])
@@ -75,7 +127,7 @@ func handle_connection(conn net.Conn) {
 					fmt.Println("error converting A")
 				}
 
-				output_img.Set(i, j, color.NRGBA{
+				first_output_img.Set(i, j, color.NRGBA{
 					R: uint8(R),
 					G: uint8(G),
 					B: uint8(B),
